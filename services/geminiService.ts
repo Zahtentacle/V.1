2d96ff01574d9c497d1701d4d4d66b96e5b4a69c
Zahtentacle <buatdownloadfilem93@gmail.com>
2026-03-17 09:20:38 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,28 +1,8 @@
 class GeminiService {
   async generatePersonaMessage(name: string, template: string): Promise<string> {
-    try {
-      // Replace placeholder {Nama}
-      let message = template.replace(/{Nama}/gi, name);
-
-      // Variasi biar natural
-      const variations = [
-        "",
-        " Jangan sampai ketinggalan ya!",
-        " Promo terbatas hari ini.",
-        " Langsung cek sekarang juga.",
-        " Kesempatan tidak datang dua kali."
-      ];
-
-      const randomVariation = variations[Math.floor(Math.random() * variations.length)];
-
-      // Random code (GHOST style)
-      const randomCode = "#" + Math.random().toString(36).substring(2, 6).toUpperCase();
-
-      return message + randomVariation + " " + randomCode;
-
-    } catch (err) {
-      return template.replace(/{Nama}/gi, name) + " #" + Math.random().toString(36).substring(2, 6).toUpperCase();
-    }
+    let msg = template.replace(/{Nama}/gi, name);
+    const code = "#" + Math.random().toString(36).substring(2, 6).toUpperCase();
+    return msg + " " + code;
   }
 }
 
